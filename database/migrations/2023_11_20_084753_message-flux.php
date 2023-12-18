520d7a621ff39d71c4651d521b40a2fe6c12c8c5
YnnsGns <gonosyannis@gmail.com>
2023-12-18 10:02:42 +0100

    modif exam

--- database/migrations/2023_11_20_084753_message-flux.php
+++ database/migrations/2023_11_20_084753_message-flux.php
@@ -22,7 +22,8 @@ return new class extends Migration
             $table->integer('retweet');
             $table->integer('like');
 
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
     }
 
