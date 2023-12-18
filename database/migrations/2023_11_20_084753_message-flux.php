535d50ce415486436141df2487fc4d08f0f2ddd6
YnnsGns <gonosyannis@gmail.com>
2023-12-18 10:04:54 +0100

    modif exam 2

--- database/migrations/2023_11_20_084753_message-flux.php
+++ database/migrations/2023_11_20_084753_message-flux.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->integer('like');
 
             $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
         });
     }
 
